446d088252bdc59ba779e0a5af15d77e28c02522
DKFAEL <rafaelnunesemp@hotmail.com>
2023-05-26 13:58:45 -0300

    Pokedex, header e search criado

---
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'poke-search',
+  templateUrl: './poke-search.component.html',
+  styleUrls: ['./poke-search.component.css']
+})
+export class PokeSearchComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+}
